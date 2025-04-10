efc262a04c633c28746219101e2c829c6699264d
enrico07 <enrico.lanza2@gmail.com>
2025-04-10 13:48:03 +0200

    implemetantion logging

--- app/endpoints/comments.py
+++ app/endpoints/comments.py
@@ -1,4 +1,3 @@
-import traceback
 from contextlib import asynccontextmanager
 from typing import List, Literal, Optional
 
@@ -7,6 +6,19 @@ from fastapi import APIRouter, FastAPI, HTTPException
 from app.handlers.comments_handler import CommentsHandler
 from app.schemas.comment_schema import Comment
 
+import logging
+
+# Configure the logger
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler("app.log", mode='a')
+    ]
+)
+logger = logging.getLogger(__name__)
+
 # Creating an instance of CommentsHandler to handle comment-related functionality
 comments_handler = CommentsHandler()
 
@@ -14,6 +26,8 @@ comments_handler = CommentsHandler()
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup: connect to DB
+    logger.info("Connecting to the database...")
+    
     await comments_handler.db_client.connect_to_db()
     yield
 
@@ -50,6 +64,9 @@ async def get_comments(
     Raises:
         HTTPException: If an unexpected error occurs during the process, a 500 error is raised.
     """
+    logger.info(f"Fetching comments for subfeddit: {subfeddit_name} with filters: "
+                f"n_comments={n_comments}, from_date={from_date}, to_date={to_date}, "
+                f"polarity_sorting={polarity_sorting}, min_polarity={min_polarity}, max_polarity={max_polarity}")
     try:
         # Call the CommentsHandler's get_comments method to fetch the comments with the specified filters
         comments = await comments_handler.get_comments(
@@ -61,8 +78,9 @@ async def get_comments(
             min_polarity=min_polarity,
             max_polarity=max_polarity,
         )
+        logger.info(f"Successfully fetched {len(comments)} comments")
     except Exception as e:
-        traceback.print_exc()
+        logger.error(f"Error while fetching comments: {str(e)}")
 
         raise HTTPException(
             status_code=500, detail=f"An unexpected error occurred: {str(e)}"
